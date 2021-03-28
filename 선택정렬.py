87dc3433fac3a4a03c5e2de44f5bfa441d6b4488
HAESOO97 <79429010+HAESOO97@users.noreply.github.com>
2021-03-28 16:56:54 +0900

    Create 선택정렬.py

---
+++ 선택정렬.py
@@ -0,0 +1,12 @@
+<선택정렬>
+이 중에서 가장 작은 데이터를 선택해 맨 앞에 있는 데이터와 바꾸고 그다음 작은 데이터를 선택해 두번째 데이터와 바꾸는 과정
+즉 매번 가장 작은것을 '선택'한다는의미에서 선택 정렬이라 한다.
+
+array = [7,5,9,0,3,1,6,2,4,8]
+for i in range(len(array)):
+  min_index = i 
+  for j in range(i+1,len(array)):
+    if array[min_index]>array[j]:
+      min_index = j
+   array[i],array[min_index] = array[min_index],array[i]
+print(array)
